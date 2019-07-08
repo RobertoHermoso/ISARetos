324db296ce1c6509355e49f2dd1f75a423b77815
Roberto Hermoso <37594160+RobertoHermoso@users.noreply.github.com>
2019-07-08 12:57:13 +0200

    Representación
    Representación gráfica de las jugadas. Indicando por consola todos los elementos de la misma

--- pokerRules.js
+++ pokerRules.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 
-let rowdata = fs.readFileSync('poker.json')
-let jugadas = JSON.parse(rowdata)
-console.log(jugadas)
+let rowdata = fs.readFileSync('poker.json', 'utf8')
+let json = JSON.parse(rowdata)
+console.log("Partidas: ")
+i = 1
+json.partida.forEach(element => {
+    console.log("Jugada número: " + i +  "\n")
+    i++
+    element.jugadas.forEach(jugadores => {
+        console.log(" - El jugador " + jugadores.jugador + " apuesta " + jugadores.apuesta + "\n")
+        console.log("Con las cartas: ")
+        console.log(jugadores.cartas)
+    });
+ 
+});
