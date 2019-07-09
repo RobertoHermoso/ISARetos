31eb5298e72896db0186fe7391fc36ad76f7b1b9
Roberto Hermoso <37594160+RobertoHermoso@users.noreply.github.com>
2019-07-09 19:26:01 +0200

    Contemplamos las parejas

--- pokerRules.js
+++ pokerRules.js
@@ -1,16 +1,77 @@
 const fs = require('fs');
+var Promise = require("bluebird");
 
 let rowdata = fs.readFileSync('poker.json', 'utf8')
 let json = JSON.parse(rowdata)
-console.log("Partidas: ")
-i = 1
-json.partida.forEach(element => {
-    console.log("Jugada número: " + i +  "\n")
-    i++
-    element.jugadas.forEach(jugadores => {
-        console.log(" - El jugador " + jugadores.jugador + " apuesta " + jugadores.apuesta + "\n")
-        console.log("Con las cartas: ")
-        console.log(jugadores.cartas)
+
+
+
+var checkCards= new Promise(
+    function(resolve,reject){
+        i = 1
+        json.partida.forEach(element => {
+            i++
+            pair = false
+            element.jugadas.forEach(jugadores => {
+                jugadores.PUNTOS = 0
+                var cartas = jugadores.cartas
+                cartas.forEach(cardPicked => {
+                    auxPair = 0
+                    cartas.forEach(cards => {
+                        //Pareja
+                        if(cards.valor == cardPicked.valor){
+                            auxPair += 1
+                        }
+                        if(auxPair == 2){
+                            pair = true
+                        }
+                    })
+                });
+                j=0
+                //Pareja
+                if(pair){
+                    jugadores.PUNTOS += 2
+                }
+                if(cartas.length != 5){
+                    var reason = new Error("El jugador " + jugadores.jugador + " está haciendo trampas, tiene " + cartas.length + " cartas "
+                    + " en la ronda " + i)
+                    reject(reason)
+                }
+                resolve(jugadores)
+            });
+        
+        });
+
+    }
+)
+
+
+var showOff = function(json, winner){
+    console.log("Partidas: ")
+    i = 1
+    json.partida.forEach(element => {
+        console.log("Jugada número: " + i +  "\n")
+        i++
+        element.jugadas.forEach(jugadores => {
+            console.log(" - El jugador " + jugadores.jugador + " apuesta " + jugadores.apuesta + "\n")
+            console.log("Con las cartas: ")
+            console.log(jugadores.cartas)
+            console.log("Lleva : " + jugadores.PUNTOS + " puntos ")
+        });
+
+            if(winner != null){
+            var message = "El jugador " + winner.jugador + " ha ganado " + winner.puntos + " puntos ";
+            console.log(message)
+            }
     });
- 
-});
+}
+
+
+var playGame = function(){
+    checkCards.then(showOff(json)).then(function (fulflilled){   
+    }).catch(function(error){
+        console.log(error.message)
+    })
+}
+
+playGame()
--- prueba.js
+++ prueba.js
@@ -1,1 +1,54 @@
-console.log('Hola mundo');
+var request = require('request')
+
+var isMomHappy = true;
+
+var willIGetNewPhone = new Promise(
+    function(resolve,reject){
+        if(isMomHappy){
+            var phone= {
+                brand: "Samsung",
+                color: "blue",
+                usd:    "700"
+            };
+            resolve(phone)
+        }
+        else{
+            var reason = new Error("Mom is not happy, so you can't buy a new phone") //Tratamiento de errores, incumplimento de la regla
+            reject(reason)
+        }
+    }
+)
+
+//Comprobacion de la regla o promesa
+
+var askMom = function(){
+    willIGetNewPhone.then(getEurPrice).then(showOff).then(function (fulfilled){ //Lo que esta debajo se seguirá ejecutando asincronamente
+    }).catch(function(error){
+        console.log(error.message)
+    })
+}
+
+var showOff = function(phone){
+    var message = "I will buy a new phone, brand " + phone.brand + ", color "+ phone.color + " and that it costs " + phone.usd + " $ " 
+    + " or " + phone.EUR + "€";
+    console.log(message)
+}
+
+
+//Forma de obtener datos desde fuera
+var getEurPrice = function(phone){
+    return new Promise(
+        function(resolve,reject){
+            request("https://api.exchangeratesapi.io/latest?base=USD", {json: true},
+            function(err, res, body){
+                if(err){
+                    return console.log(err);
+                }
+                phone.EUR = phone.usd*body.rates.EUR
+                resolve(phone)
+            })
+        }
+    )
+}
+
+askMom()
