5ccf7d4ad7791f96b23162cc9021b820778b1f12
Roberto Hermoso <37594160+RobertoHermoso@users.noreply.github.com>
2019-07-21 13:19:20 +0200

    Mas infromación acerca de los repositorios
    Me falta quitar la limitación de resultados o en su defecto poder iterar sobre cada pagian de resultados

--- gitHub/testing.js
+++ gitHub/testing.js
@@ -4,19 +4,27 @@ const https = require('https');
 //Aquí se introduce lo que hemos pasado por consola
 let org = 'isa-group'
 
-let user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-             + ' OPR/62.0.3331.81'
+let user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36 OPR/62.0.3331.81'
 
+let token = "cd6b55ad950a81fcb255caa034158cf9a3154fa6"
+
+let host = "api.github.com"
+
+let limit = 200
+
+var mapRepositories = new Map()
 
 var information = new Promise(
     function(resolve,reject){
 
 
 let options = {
-    host: 'api.github.com',
+    host: host,
     path: '/orgs/' + org,
     method: 'GET',
-    headers: { 'user-agent': user_agent}
+    headers: {  'user-agent': user_agent,
+                'client_id' : 'RobertoHermoso',
+                'Authorization' : "Bearer " + token}
 }
 
 let request = https.request(options , (response) => {
@@ -49,10 +57,13 @@ var repositories = function(json){
         function(resolve,reject){
             
             let options = {
-                host: "api.github.com",
-                path: '/orgs/' + org + "/" + "repos",
+                host: host,
+                path: '/orgs/' + org + "/" + "repos?per_page=" + limit,
                 method: 'GET',
-                headers: { 'user-agent': user_agent}
+                per_page: limit,
+                headers: { 'user-agent': user_agent,
+                'client_id' : 'RobertoHermoso',
+                'Authorization' : "Bearer " + token}
             }
 
             let request = https.request(options , (response) => {
@@ -82,29 +93,18 @@ var repositories = function(json){
     )
 }
 
-var showInf = function(json){
-    var message = "Nombre: " + json.name + "\n" + "Descripción: " + json.description + "\n" + "Enlace: " + json.blog + "\n\n" +
-     "Repositorios: \n\n" ;
-    console.log(message)
-
-    var repos = json.repos
-    repository = repos[0]
-        var name = repository.name
-        console.log(" - " + name)
-        getIssues(name);
-
+function getRepoInfo(repository){
         
-}
-
- function getIssues(repository){
-
     var i = 0;
 
     let options = {
-        host: "api.github.com",
-        path: '/repos/' + org + "/" + repository + "/issues",
+        host: host,
+        path: '/repos/' + org + "/" + repository.name + "/commits?per_page=" + limit ,
         method: 'GET',
-        headers: { 'user-agent': user_agent}
+        headers: { 'user-agent': user_agent,
+        'client_id' : 'RobertoHermoso',
+        'Authorization' : "Bearer " + token,
+        'limit': 99999}
     }
 
     let request = https.request(options , (response) => {
@@ -114,14 +114,14 @@ var showInf = function(json){
         });
     
         response.on('end', (out) => {
-            var issues = JSON.parse(body);
-            issues.forEach(is => {
-                if(is.state=="open"){
-                    i++
-                }
-            });
-            var message = "       Número de Issues abiertas " + i
-            return message
+            json = JSON.parse(body);
+            var i = json.length
+            console.log("-"  +repository.name )
+            console.log("   Numero de Issues abiertas : " + repository.open_issues_count)
+            if(i == undefined){
+                i = 0
+            }
+            console.log("   Número de Commits: " + i + "\n")
         });
     
         
@@ -130,10 +130,26 @@ var showInf = function(json){
             var message = new Error("Error getting the issues from " +repository) //Tratamiento de errores
             console.log(message)
     });
-
-    request.end();
-
+    request.end();   
 }
+
+var showInf = function(json){
+    var message = "Nombre: " + json.name + "\n" + "Descripción: " + json.description + "\n" + "Enlace: " + json.blog + "\n\n" +
+     "Repositorios: \n" ;
+    console.log(message)
+
+    var repos = json.repos
+    console.log(repos.length)
+    repos.forEach(element => {
+        getRepoInfo(element)
+    });
+
+        
+}
+
+
+
+
 var execute = function(){
     information.then(repositories).then(showInf).then(function (fulfilled){ //Lo que esta debajo se seguirá ejecutando asincronamente
     }).catch(function(error){
